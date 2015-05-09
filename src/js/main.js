57459359f9a3bf5b0bddb23178e29456395aec91
Alexey Nikiforov <Avetome@gmail.com>
2015-05-09 20:31:24 +0400

    ractive-backbone rope proto

--- src/js/main.js
+++ src/js/main.js
@@ -3,18 +3,29 @@ window.jQuery = window.$ = jQuery = $ = require("jquery");
 var Issue = require("./models/issue");
 var Issues = require("./collections/issues");
 
-var Greeter = function() {};
+var Backbone = require("backbone");
+var Ractive = require("ractive");
+var BackboneAdaptor = require("ractive-adaptors-backbone");
+BackboneAdaptor.Backbone = Backbone;
 
-Greeter.prototype.sayHello = function(name) {
-    console.debug("Hello, " + name);
-}
+var issues = new Issues([
+                new Issue({number: 800, title: "test issue"}), 
+                new Issue({number: 900, title: "test issue 2"})
+            ]);
+
+var IssuesListView = Ractive.extend({
+    oninit: function (options) {
+    }  
+});
 
 window.onload = function() {
-    var greeter = new Greeter();
-    greeter.sayHello("World");
 
-    console.debug(new Issues([
-            new Issue({number: 800, title: "test issue"}), 
-            new Issue({number: 900, title: "test issue 2"})
-    ]));
+    var issuesListView = new IssuesListView({
+        el: "#IssuesList",
+        template: "#IssuesListTemplate",
+        data: {issues: issues},
+        adapt: [ BackboneAdaptor ],
+    });
+
+    console.debug(issues);
 }
