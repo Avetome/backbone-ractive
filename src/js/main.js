961107806cf8e8eac09b05cb09c925c4c4c77666
Alexey Nikiforov <Avetome@gmail.com>
2015-05-10 04:10:43 +0400

    Move IssuesListView to separate file

--- src/js/main.js
+++ src/js/main.js
@@ -1,87 +1,9 @@
-window.jQuery = window.$ = jQuery = $ = require("jquery");
-
-var Issue = require("./models/issue");
-var Issues = require("./collections/issues");
-var Repositories = require("./collections/repositories");
-var Urls = require("./utils/urls");
-
-var Backbone = require("backbone");
-Backbone.$ = $;
-var Ractive = require("ractive");
-var BackboneAdaptor = require("ractive-adaptors-backbone");
-BackboneAdaptor.Backbone = Backbone;
-
-var issues = new Issues([
-                new Issue({number: 800, title: "test issue"}), 
-                new Issue({number: 900, title: "test issue 2"})
-            ]);
-
-var IssuesListView = Ractive.extend({
-    oninit: function (options) {
-        this.on({
-            userChange: function(event) {
-                var repos = this.get("repositories");
-                repos.url = Urls.repos(this.get("user"));
-                this.set("reposLoading", true);
-
-                repos.fetch()
-                    .done(function(){
-                        this.set("repositories", repos);
-                        this.set("userHasNoRepos", !repos.length);
-                        this.set("errorLoadingRepos", false);
-                        this.set("reposLoading", false);
-                    }.bind(this))
-                    .fail(function(error){
-                        this.set("userHasNoRepos", false);
-                        this.set("errorLoadingRepos", true);
-                        this.set("reposLoading", false);
-                    }.bind(this)
-                );
-
-                // clean issues list
-                var issues = this.get("issues");
-                issues.reset([]);
-                this.set("issues", issues);
-            },
-
-            repoChange: function(event) {
-                var id = event.original.target.value;
-                var repository = this.get("repositories").get(id);
-                this.set("repository", repository);
-                var issues = this.get("issues");
-
-                // also we can user repository.get("issues_url"), but in this case we need cut "{/number}" substring                
-                issues.url = Urls.issues(this.get("user"), repository.get("name"));
-                issues.fetch()
-                    .done(function(){
-                        this.set("issues", issues);
-                    }.bind(this))
-                    .fail(function(error){
-                        console.error(error);
-                    }.bind(this)                    
-                );
-            }
-        });
-    }  
-});
+var IssuesListView = require("./views/IssuesListView");
 
 window.onload = function() {
 
     var issuesListView = new IssuesListView({
         el: "#IssuesList",
         template: "#IssuesListTemplate",
-        data: {
-            issues: issues,
-            user: "",
-            repository: "",
-            repositories: new Repositories(),
-            userHasNoRepos: false,
-            errorLoadingRepos: false,
-            reposLoading: false
-        },
-
-        adapt: [ BackboneAdaptor ],
     });
-
-    console.debug(issues);
 }
